2da3014c45c3db9ec4af913721d035cc7c894017
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-06-11 13:47:15 +0900

    캐릭터 체력바 추가

---
+++ Assets/HpBarWorld.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpBarWorld : MonoBehaviour
+{
+    public Image _background;
+    public Image _mask;
+
+    public MyCharacterController _characterControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHpBarStatus();
+    }
+
+    void UpdateHpBarStatus()
+    {
+        float currentHp = _characterControllerScript.CurrentHp;
+        float maxHp = _characterControllerScript.MaxHp;
+        float lerpSpeed = 5f;
+
+        // 체력 텍스트 표시
+        //_hpStringState.text = string.Format("{0} / {1}", currentHp, maxHp);
+
+        // // mask의 height는 변하지 않을거라서 그냥 가져온다.
+        // float height = _mask.GetComponent<RectTransform>().sizeDelta.y;
+        //
+        // // background의 width값이 x인데 이게 최대 크기이니까 fullWidth라 명명한다.
+        // float fullWidth = _background.GetComponent<RectTransform>().sizeDelta.x;
+        //
+        // // hp / maxHp => 0~1사이의 값을 갖게되고 0.5 * fullWidth하게 되면 => 절반으로 마스킹 사이즈가 된다.
+        // _mask.GetComponent<RectTransform>().sizeDelta = new Vector2(currentHp / maxHp * fullWidth, height);
+
+        float fillAmount = currentHp / maxHp;
+        _mask.fillAmount = Mathf.Lerp(_mask.fillAmount, fillAmount, Time.deltaTime * lerpSpeed);
+    }
+}
