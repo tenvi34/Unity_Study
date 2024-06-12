2e64f4336b2315a4ed8c43b3f3b1048492adb8ca
Jiwon Park <64686238+tenvi34@users.noreply.github.com>
2024-06-12 11:38:02 +0900

    Delegate없이

--- Assets/20240612/delegateExample.cs
+++ Assets/20240612/delegateExample.cs
@@ -2,109 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 델리게이트 제작
-public delegate void CalculationCompletedEventHandler(int result);
-public delegate void CalculationCompletedEventHandler2(int result);
-
-public class TestClass
+namespace delegateExampleNS
 {
-    public CalculationCompletedEventHandler2 CalculationCompleted2;
-    
-    public void TestClassFunc(int result)
-    {
-        Debug.Log($"result : {result}");
-    }
+    // 델리게이트 제작
+    public delegate void CalculationCompletedEventHandler(int result);
 
-    public void AddResult(int result)
-    {
-        Debug.Log($"AddResult Func : {result}");
-    }
-    
-    public void SubtractResult(int result)
-    {
-        Debug.Log($"SubtractResult Func : {result}");
-    }
-}
+    public delegate void CalculationCompletedEventHandler2(int result);
 
-public class Calculator
-{
-    // 델리게이트 선언
-    public CalculationCompletedEventHandler CalculationCompleted;
+    public class TestClass
+    {
+        public CalculationCompletedEventHandler2 CalculationCompleted2;
 
-    // Add
-    public void Add(int a, int b)
-    {
-        int result = a + b;
-        CalculationCompleted?.Invoke(result);
-    }
-
-    // Subtract
-    public void Subtract(int a, int b)
-    {
-        int result = a - b;
-        CalculationCompleted?.Invoke(result);
-    }
-}
-public class delegateExample : MonoBehaviour
-{
-    private Calculator _calculator;
-    private int sumResult = 0;
-    
-    public TestClass testClass;
-
-    // ReSharper disable Unity.PerformanceAnalysis
-    IEnumerator CalculatorDelayPrint()
-    {
-        yield return new WaitForSeconds(10.0f);
-        
-        testClass?.CalculationCompleted2?.Invoke(10);
-    }
-    
-    void Start()
-    {
-        _calculator = new Calculator();
-        testClass = new TestClass();
-        
-        // delegateExample의 클래스에 CalculationCompletedEventHandler 함수를 등록
-        _calculator.CalculationCompleted += CalculationCompletedEventHandler;
-        _calculator.CalculationCompleted += testClass.TestClassFunc;
-
-        // 코루틴 호출
-        StartCoroutine(CalculatorDelayPrint());
-    }
-
-    void CalculationCompletedEventHandler(int result)
-    {
-        Debug.Log($"Calculation completed. Result: {result}");
-        
-        // sumResult의 변수값을 결과값으로 변경
-        sumResult = result;
-    }
-    
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        public void TestClassFunc(int result)
         {
-            _calculator?.Add(sumResult, 10);
+            Debug.Log($"result : {result}");
         }
 
-        if (testClass != null)
+        public void AddResult(int result)
         {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                testClass.CalculationCompleted2 += testClass.AddResult;
-            }
-            else if (Input.GetKeyDown(KeyCode.S))
-            {
-                testClass.CalculationCompleted2 += testClass.SubtractResult;
-            }
+            Debug.Log($"AddResult Func : {result}");
         }
-        
-        // 델리게이트에서 빼기
-        if (sumResult >= 100 && _calculator is { CalculationCompleted: not null })
+
+        public void SubtractResult(int result)
         {
-            _calculator.CalculationCompleted -= CalculationCompletedEventHandler;
-            Debug.Log("Finish");
+            Debug.Log($"SubtractResult Func : {result}");
+        }
+    }
+
+    public class Calculator
+    {
+        // 델리게이트 선언
+        public CalculationCompletedEventHandler CalculationCompleted;
+
+        // Add
+        public void Add(int a, int b)
+        {
+            int result = a + b;
+            CalculationCompleted?.Invoke(result);
+        }
+
+        // Subtract
+        public void Subtract(int a, int b)
+        {
+            int result = a - b;
+            CalculationCompleted?.Invoke(result);
+        }
+    }
+
+    public class delegateExample : MonoBehaviour
+    {
+        private Calculator _calculator;
+        private int sumResult = 0;
+
+        public TestClass testClass;
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        IEnumerator CalculatorDelayPrint()
+        {
+            yield return new WaitForSeconds(10.0f);
+
+            testClass?.CalculationCompleted2?.Invoke(10);
+        }
+
+        void Start()
+        {
+            _calculator = new Calculator();
+            testClass = new TestClass();
+
+            // delegateExample의 클래스에 CalculationCompletedEventHandler 함수를 등록
+            _calculator.CalculationCompleted += CalculationCompletedEventHandler;
+            _calculator.CalculationCompleted += testClass.TestClassFunc;
+
+            // 코루틴 호출
+            StartCoroutine(CalculatorDelayPrint());
+        }
+
+        void CalculationCompletedEventHandler(int result)
+        {
+            Debug.Log($"Calculation completed. Result: {result}");
+
+            // sumResult의 변수값을 결과값으로 변경
+            sumResult = result;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _calculator?.Add(sumResult, 10);
+            }
+
+            if (testClass != null)
+            {
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    testClass.CalculationCompleted2 += testClass.AddResult;
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    testClass.CalculationCompleted2 += testClass.SubtractResult;
+                }
+            }
+
+            // 델리게이트에서 빼기
+            if (sumResult >= 100 && _calculator is { CalculationCompleted: not null })
+            {
+                _calculator.CalculationCompleted -= CalculationCompletedEventHandler;
+                Debug.Log("Finish");
+            }
         }
     }
 }
--- Assets/20240612/normalFunctionExample.cs
+++ Assets/20240612/normalFunctionExample.cs
@@ -2,17 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class normalFunctionExample : MonoBehaviour
+namespace normalFunctionExampleNS
 {
-    // Start is called before the first frame update
-    void Start()
+    public class TestClass
     {
-        
+        public void AddResult(int result)
+        {
+            Debug.Log($"AddResult Func : {result}");
+        }
+
+        public void SubtractResult(int result)
+        {
+            Debug.Log($"SubtractResult Func : {result}");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public class Calculator
     {
+        public string functionName = string.Empty;
         
+        public TestClass testClass;
+        // Add
+        public void Add(int a, int b)
+        {
+            int result = a + b;
+            testClass.AddResult(result);
+        }
+
+        // Subtract
+        public void Subtract(int a, int b)
+        {
+            int result = a - b;
+            testClass.SubtractResult(result);
+        }
+        
+        public IEnumerator CalculatorDelayPrint()
+        {
+            yield return new WaitForSeconds(10.0f);
+
+            if (functionName == "Add")
+            {
+                Add(3, 4);
+            }
+            else if (functionName == "Subtract")
+            {
+                Subtract(12, 5);
+            }
+        }
+    }
+
+    public class normalFunctionExample : MonoBehaviour
+    {
+        private Calculator _calculator;
+        private int sumResult = 0;
+
+        public TestClass testClass;
+        
+        void Start()
+        {
+            _calculator = new Calculator();
+            testClass = new TestClass();
+
+            _calculator.testClass = testClass;
+
+            StartCoroutine(_calculator.CalculatorDelayPrint());
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                _calculator.functionName = "Add";
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                _calculator.functionName = "Subtract";
+            }
+        }
     }
 }
